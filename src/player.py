17dc8fc4da30a6903b4d87a82f968c1c43245413
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-09-02 16:10:18 -0700

    Added another comment

--- src/player.py
+++ src/player.py
@@ -1,4 +1,4 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-# Added this comment to be able to make a pull request.
+# Added another comment to be able to make a pull request.
