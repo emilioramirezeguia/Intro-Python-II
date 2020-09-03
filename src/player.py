1c5e7e382334a06fc59bdbf31923e4a75616086a
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-09-03 15:42:29 -0700

    Create Item clas

--- src/player.py
+++ src/player.py
@@ -1,7 +1,5 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-
-
 class Player:
     def __init__(self, name, current_room, items=None):
         self.name = name
